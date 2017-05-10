a95460b65e44db813c82535f468218db39c2ca5b
Simon Effing <simon.effing@gmail.com>
2017-05-10 20:15:45 +0200

    removed closing brs

--- en/assets/js/script.js
+++ en/assets/js/script.js
@@ -1,5 +1,5 @@
 function menu() {
-	$(".menu,.nav .descubrenos,.nav .about,.nav .work,.nav .contact,.nav .modalities,.nav .data,.nav .preise").click(function(){
+	$(".menu,.nav .home,.nav .about,.nav .work,.nav .contact,.nav .modalities,.nav .data,.nav .preise").click(function(){
 		$(".nav-section").toggleClass("show");
 		$(".menu .nav-bar:nth(0)").toggleClass("animate-0");
 		$(".menu .nav-bar:nth(1)").toggleClass("animate-1");
