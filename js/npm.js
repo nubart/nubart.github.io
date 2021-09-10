45fc907bf67d19f330660d4871eb4db8ed7b88eb
rosa <rosa.sala.rose@gmail.com>
2021-09-10 12:14:00 +0200

    carousel.js minified

--- js/npm.js
+++ js/npm.js
@@ -2,7 +2,7 @@
 require('../../js/transition.js')
 require('../../js/alert.js')
 require('../../js/button.js')
-require('../../js/carousel.js')
+require('../../js/carousel.min.js')
 require('../../js/collapse.js')
 require('../../js/dropdown.js')
 require('../../js/modal.js')
