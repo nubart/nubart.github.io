23e3b4e1154334b35f51744c65ec0ee3f47ca01f
simon <simon.effing@gmail.com>
2024-11-26 13:03:36 +0100

    consent spike

---
+++ pl/cookieconsent-config.js
@@ -0,0 +1,23 @@
+import 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.umd.js';
+
+CookieConsent.run({
+    categories: {
+        analytics: {
+            enabled: false
+        }
+    },
+    language: {
+        default: 'en',
+        translations: {
+            en: {
+                consentModal: {
+                    title: 'We use cookies',
+                    description: 'Cookie modal description',
+                    acceptAllBtn: 'Accept all',
+                    acceptNecessaryBtn: 'Reject all',
+                    showPreferencesBtn: 'Manage Individual preferences'
+                },
+            }
+        }
+    }
+});
