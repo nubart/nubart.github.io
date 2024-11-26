cc2fc4aa4f5112948c63b124df1de3d422d49b9c
simon <simon.effing@gmail.com>
2024-11-26 14:32:51 +0100

    wording

--- pl/cookieconsent-config.js
+++ pl/cookieconsent-config.js
@@ -11,10 +11,11 @@ CookieConsent.run({
         translations: {
             en: {
                 consentModal: {
-                    title: 'We use cookies',
-                    description: 'Cookie modal description',
-                    acceptAllBtn: 'Accept all',
-                    acceptNecessaryBtn: 'Reject all',
+                    title: "This website stores cookies on your computer. These cookies are used to improve your website experience and provide more personalized services to you, both on this website and through other media. " +
+                        "To find out more about the cookies we use, see our Privacy Policy.",
+                    description: "We won't track your information when you visit our site. But in order to comply with your preferences, we'll have to use just one tiny cookie so that you're not asked to make this choice again.",
+                    acceptAllBtn: 'Accept',
+                    acceptNecessaryBtn: 'Decline'
                 },
             }
         }
