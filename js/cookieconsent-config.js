1fe269be786d33b883dfe2d0c223ddfa90204ae4
simon <simon.effing@gmail.com>
2024-11-27 19:51:51 +0100

    delete cookies on revoke

--- js/cookieconsent-config.js
+++ js/cookieconsent-config.js
@@ -1,9 +1,15 @@
-import {run} from 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.esm.js';
+import {run, acceptedCategory} from 'https://cdn.jsdelivr.net/gh/orestbida/cookieconsent@3.0.1/dist/cookieconsent.esm.js';
 
 run({
     categories: {
         analytics: {
             enabled: false
+        }
+    },
+    onChange: (cookie) => {
+        if(!acceptedCategory('analytics')){
+            deleteCookies();
+            window.location.reload();
         }
     },
     guiOptions: {
@@ -110,3 +116,15 @@ run({
         }
     }
 });
+
+function deleteCookies() {
+    const cookies = document.cookie.split(";");
+    const domain = window.location.hostname;
+
+    for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i];
+        const eqPos = cookie.indexOf("=");
+        const name = eqPos > -1 ? cookie.slice(0, eqPos).trim() : cookie.trim();
+        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 GMT;path=/;domain=${domain}`;
+    }
+}
